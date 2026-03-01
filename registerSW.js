b60b5666a01eec7784cc3cf6b809cb5111d19f38
psnpspgnb-wq <psn.pspg.nb@gmail.com>
2026-03-01 13:45:08 +0900

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/inventory-pwa/sw.js', { scope: '/inventory-pwa/' })})}
